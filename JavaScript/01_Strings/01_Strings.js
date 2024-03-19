d7d5522ab4741a9c394832877d0df6f945f9d432
dabhikartik <dabhikartik5533@gmail.com>
2024-03-19 12:15:14 +0530

    complete

--- JavaScript/01_Strings/01_Strings.js
+++ JavaScript/01_Strings/01_Strings.js
@@ -1,14 +1,14 @@
 // Write a program to print string
 
 /*
- 0101-Print a String - Write a program which print below outout when program run/executed.
+//0101-Print a String - Write a program which print below outout when program run/executed.
 
 let companyName = "Rudra IT Hub"
 
 console.log(companyName)
 
 
-0102-length of a String - Write a program to find a lenght of a string and disply lenthg when program run/executed.
+//0102-length of a String - Write a program to find a lenght of a string and disply lenthg when program run/executed.
 
 let name = "WelCome To Rudra IT Hub"
 
@@ -16,7 +16,7 @@ console.log(name)
 let length = name.length;
 console.log(length)
 
-0103-join a string - Write a program to join two string.
+//0103-join a string - Write a program to join two string.
 
 let string1="Hello"
 let string2="world"
@@ -24,44 +24,43 @@ let string2="world"
 let string3=string1.concat(" ",string2)
 console.log(string3)
 
-0104 - write a program print getting the first element of the String.
+//0104 - write a program print getting the first element of the String.
 
 let char=string3[0];
 
 console.log(char)
 
-0105 - write a program separate individual characters from a string.
+//0105 - write a program separate individual characters from a string.
 let name1 = "Rudra IT Hub.com";
 
 let text = name1.split('');
 console.log(text)
 
-0106 - write a program to print individual characters of a string in reverse order.
+//0106 - write a program to print individual characters of a string in reverse order.
 
 // let element = "Rudra";
-// let reversedString =
 // console.log(element.split('').reverse().join(""))
 
-0107 - write a program to count the total number of words in a string.
+//0107 - write a program to count the total number of words in a string.
 let word="This is Rudra IT Hub.com";
 let words=word.split(" ").length;
 
 console.log(words)
 
-## 0108 - write a program to to convert a string to lowercase.
+//0108 - write a program to to convert a string to lowercase.
 
 let stringUPPERCAS= "WELCOME TO RUDRA IT HUB";
 let stringLowercas=stringUPPERCAS.toLocaleLowerCase();
 console.log(stringLowercas)
 
-0109 - write a program to replace a specific word or character in a text string with another word or character.
+//0109 - write a program to replace a specific word or character in a text string with another word or character.
 
 let name2="Rudra";
 let newname2=name2.replace("Rudra","It Hub");
 
 console.log(newname2)
 
-## 0110 - Create a program that extracts a substring from a string based on the starting and ending positions provided.
+//0110 - Create a program that extracts a substring from a string based on the starting and ending positions provided.
 
 
 let name3="Rudra";
